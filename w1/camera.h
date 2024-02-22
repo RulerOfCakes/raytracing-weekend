e1bf4b954ae7838306968ca6b2f44aaad13d05d9
RulerOfCakes <fragmentbombs@gmail.com>
2024-02-22 21:15:39 +0900

    feat: metallic reflection

--- w1/camera.h
+++ w1/camera.h
@@ -5,6 +5,7 @@
 
 #include "color.h"
 #include "hittable.h"
+#include "material.h"
 
 #include <iostream>
 
@@ -83,9 +84,12 @@ private:
       return color(0, 0, 0);
     hit_record rec;
     if (world.hit(r, interval(0.001, infinity), rec)) {
-      // color by unit normals
-      vec3 direction = rec.normal + random_unit();
-      return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+      ray scattered;
+      color attenuation;
+      if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+        return attenuation * ray_color(scattered, depth - 1, world);
+      }
+      return color(0, 0, 0);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
--- w1/vec3.h
+++ w1/vec3.h
@@ -43,6 +43,11 @@ public:
     return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
   }
 
+  bool near_zero() const {
+    auto eps = 1e-8;
+    return (fabs(e[0]) < eps) && (fabs(e[1]) < eps) && (fabs(e[2]) < eps);
+  }
+
   static vec3 random() {
     return vec3(random_double(), random_double(), random_double());
   }
@@ -119,5 +124,7 @@ inline vec3 random_on_hemisphere(const vec3 &normal) {
     return -on_unit_sphere;
   }
 }
+
+vec3 reflect(const vec3 &v, const vec3 &n) { return v - 2 * dot(v, n) * n; }
 
 #endif
